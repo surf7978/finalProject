00f98621bf680fd5583f36a685334c0062e00813
surf7 <surf7@DESKTOP-CDL40RC>
2021-03-27 18:03:00 +0900

    VO추가

---
+++ finalProject/src/main/java/com/company/animal/service/AnimalVO.java
@@ -0,0 +1,13 @@
+package com.company.animal.service;
+
+import lombok.Data;
+
+@Data
+public class AnimalVO {
+	String animalNumber;
+	String name;
+	String gender;
+	String kind;
+	String birth;
+	String memberId;
+}
