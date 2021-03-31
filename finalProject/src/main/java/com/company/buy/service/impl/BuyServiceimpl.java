1d9628ef58eeb607321077abc662a74740a6405c
haltp <namoo3605@naver.com>
2021-03-31 10:54:14 +0900

    새로운 시작...

--- finalProject/src/main/java/com/company/buy/service/impl/BuyServiceimpl.java
+++ finalProject/src/main/java/com/company/buy/service/impl/BuyServiceimpl.java
@@ -10,34 +10,19 @@ import com.company.buy.service.BuyVO;
 
 @Service
 public class BuyServiceimpl implements BuyService{
+
+	@Autowired BuyMapper buyMapper;
 	
-	@Autowired BuyMapper BuyMapper;
-	//등록
-	@Override
-	public int insertBuy(BuyVO vo) {
-		return BuyMapper.insertBuy(vo);
-	}
-	//단건조회
-	@Override
-	public BuyVO getBuy(BuyVO vo) {
-		return BuyMapper.getBuy(vo);
-	}
-	//전체조회
+	//구매내역 상세리스트 조회
 	@Override
 	public List<BuyVO> getSearchBuy(BuyVO vo) {
-		return BuyMapper.getSearchBuy(vo);
-	}
-	//수정
-	@Override
-	public int updateBuy(BuyVO vo) {
-		return BuyMapper.updateBuy(vo);
+		return buyMapper.getSearchBuy(vo);
 	}
 	
-	//탈퇴
+	//구매 한건 삭제
 	@Override
 	public int deleteBuy(BuyVO vo) {
-		return BuyMapper.deleteBuy(vo);
+		return buyMapper.deleteBuy(vo);
 	}
-
 
 }
--- finalProject/src/main/java/com/company/payAndDelivery/service/PayAndDeliveryService.java
+++ finalProject/src/main/java/com/company/payAndDelivery/service/PayAndDeliveryService.java
@@ -3,9 +3,9 @@ package com.company.payAndDelivery.service;
 import java.util.List;
 
 public interface PayAndDeliveryService {
-	public int insertPayAndDelivery(PayAndDeliveryVO vo);	//화원탈퇴
-	public PayAndDeliveryVO getPayAndDelivery(PayAndDeliveryVO vo);	//단건조회
-	public List<PayAndDeliveryVO> getSearchPayAndDelivery(PayAndDeliveryVO vo);	//전체조회
-	public int updatePayAndDelivery(PayAndDeliveryVO vo);	//회원수정
-	public int deletePayAndDelivery(PayAndDeliveryVO vo);	//화원탈퇴
+	public int insertPayAndDelivery(PayAndDeliveryVO vo);
+	public PayAndDeliveryVO getPayAndDelivery(PayAndDeliveryVO vo);
+	public List<PayAndDeliveryVO> getSearchPayAndDelivery(PayAndDeliveryVO vo); 	//구매내역 조회(일반회원)
+	public int updatePayAndDelivery(PayAndDeliveryVO vo);	
+	public int deletePayAndDelivery(PayAndDeliveryVO vo);	//구매내역 삭제(일반회원)
 }
