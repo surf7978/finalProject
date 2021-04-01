e9df3c2b9c547e59064f08417e3d8173b96f4398
surf7978 <surf7978@naver.com>
2021-04-01 17:36:31 +0900

    휴대폰인증 추가, Answer 조회 추가

--- finalProject/src/main/java/com/company/controller/Controller1.java
+++ finalProject/src/main/java/com/company/controller/Controller1.java
@@ -194,7 +194,7 @@ public class Controller1 {
         certificationService.certifiedPhoneNumber(phoneNumber,numStr);
         return numStr;
     }
-	
+	 
 	// 홈화면 출력(스프링 기본세팅)
 	private static final Logger logger = LoggerFactory.getLogger(Controller1.class);
 	@RequestMapping(value = "/", method = RequestMethod.GET)
