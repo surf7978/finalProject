30c34e55208659f6d416840946e342c13b05efea
박세민 <qseft523125@naver.com>
2021-04-09 12:53:20 +0900

    0409오전

--- finalProject/src/main/java/com/company/controller/Controller5.java
+++ finalProject/src/main/java/com/company/controller/Controller5.java
@@ -44,7 +44,7 @@ import com.company.question.service.QuestionVO;
  * 21.04.06 사업체-카페-전체리스트 3차 수정(Ajax,paging)
  * 21.04.07 사업체-통합 리스트(Ajax,paging,search,checkbox)
  * 21.04.08 사업체-통합 리스트 세분화(카테고리 별 검색 완)
- * 21.04.09 Oracle Cloud DB 설정/
+ * 21.04.09 Oracle Cloud DB 설정/ 사업자-통합 리스트(checkbox 여러개 채크시 포함되는 결과 전부 나오도록 변경)
  */
 @Controller
 public class Controller5 {
