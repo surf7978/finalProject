c10785d954154a8b4af1e94bdcf247fc1bfb33a5
박세민 <qseft523125@naver.com>
2021-04-19 11:45:23 +0900

    0419 장바구니 6차 수정(단건삭제 ok,여러건 삭제 수정중)

--- finalProject/src/main/java/com/company/bCart/service/BCartService.java
+++ finalProject/src/main/java/com/company/bCart/service/BCartService.java
@@ -5,13 +5,13 @@ import java.util.List;
 import com.company.cart.service.CartVO;
 
 public interface BCartService {
-	public int insertBCart(BCartVO vo); // 화원탈퇴
+	public int insertBCart(BCartVO vo); // 등록
 
-	public int updateBCart(BCartVO vo); // 회원수정
+	public int updateBCart(BCartVO vo); // 수정
 
-	public int deleteBCart(BCartVO vo); // 화원탈퇴
+	public int deleteBCart(BCartVO vo); // 삭제
 
 	public BCartVO getBCart(BCartVO vo); // 단건조회
 
-	public List<BCartVO> getSearchTotalCart(CartVO vo); // 전체조회
+	public List<CartVO> getSearchTotalCart(CartVO vo); // 전체조회
 }
